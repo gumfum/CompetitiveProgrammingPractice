73a794d21385b17ae3761c9e1275f92fe56c09ab
Ryohei Takasawa <henshinfukanou@gmail.com>
2014-07-25 15:55:57 +0900

    Modify Problem 15 using combination.

--- ProjectEuler/Problem15.hs
+++ ProjectEuler/Problem15.hs
@@ -1,1 +1,4 @@
-main = print $ div (product [21..40]) (product [1..20])
+combination :: Integer -> Integer -> Integer
+combination m n = div (product [m-n+1..m]) (product [1..n])
+
+main = print $ combination 40 20
