210a288fa11b7e0636b109c5f2c943a1c4662b48
Ryohei Takasawa <henshinfukanou@gmail.com>
2014-09-30 15:23:34 +0900

    Add Problem 67.

---
+++ ProjectEuler/Problem67.hs
@@ -0,0 +1,19 @@
+import Data.List.Split
+
+toInt :: String -> Int
+toInt str = read str :: Int
+
+toIntList :: [String] -> [Int]
+toIntList list = map toInt list
+
+sumMax :: Int -> Int -> Int -> Int
+sumMax a b c = a + max b c
+
+sumMaxList :: [Int] -> [Int] -> [Int]
+sumMaxList xs ys = zipWith3 sumMax xs ys (tail ys)
+
+main = do
+	str <- readFile "triangle.txt"
+	let list = map (splitOn " ") (lines str)
+	let intList = map toIntList list
+	print $ head (foldr1 sumMaxList intList)
