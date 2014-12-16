ca01e5d4351a0625f4577de859725a9e0dea59d5
Ryohei Takasawa <henshinfukanou@gmail.com>
2014-12-17 03:19:05 +0900

    Add AOJ1335 file.

---
+++ AizuOnlineJugde/AOJ13xx/AOJ1335/EqualSumSets.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <climits>
+#include <fstream>
+using namespace std;
+inline int toInt(string s) { int v; istringstream sin(s); sin >> v; return v; }
+template<class T> inline string toStr(T x) { ostringstream sout; sout << x; return sout.str(); }
+typedef vector<int> vi;
+typedef vector<vi>  vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define REP(i,n) FOR(i,0,(n)-1)
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+const int INF = INT_MAX / 10;
+
+int main() {
+	int n, k, s;
+	while (cin >> n >> k >> s, n) {
+		vvi dp(k + 1, vi(s + 1));
+		dp[0][0] = 1;
+		FOR(i, 1, n) {
+			for (int j = k; j > 0; j--) {
+				FOR(l, i, s) {
+					dp[j][l] += dp[j - 1][l - i];
+				}
+			}
+		}
+		cout << dp[k][s] << endl;
+	}
+
+	return 0;
+}
