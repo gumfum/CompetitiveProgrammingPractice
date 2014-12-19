409fa154853881cb77b5da24cb69ba247fc13f34
Ryohei Takasawa <henshinfukanou@gmail.com>
2014-12-19 15:10:14 +0900

    Add AOJ549 file.

---
+++ AizuOnlineJugde/AOJ05xx/AOJ0549/ATraveler.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <climits>
+#include <fstream>
+using namespace std;
+inline int toInt(string s) { int v; istringstream sin(s); sin >> v; return v; }
+template<class T> inline string toStr(T x) { ostringstream sout; sout << x; return sout.str(); }
+typedef vector<int> vi;
+typedef vector<vi>  vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define REP(i,n) FOR(i,0,(n)-1)
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+const int INF = INT_MAX / 10;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vi dist(n + 1);
+	FOR(i, 2, n) {
+		cin >> dist[i];
+		dist[i] += dist[i - 1];
+	}
+
+	int s, p = 1, ans = 0;
+	REP(i, m) {
+		cin >> s;
+		int np = p + s;
+		ans += abs(dist[p] - dist[np]);
+		ans %= 100000;
+		p = np;
+	}
+	cout << ans << endl;
+	return 0;
+}
