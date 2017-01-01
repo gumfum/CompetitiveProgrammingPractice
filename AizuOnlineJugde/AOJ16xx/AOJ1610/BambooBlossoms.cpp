5591e853ab1f74617279c51e73710f3b29222cf7
Ryohei Takasawa <henshinfukanou@gmail.com>
2017-01-01 17:15:02 +0900

    Add AOJ1610 file.

---
+++ AizuOnlineJugde/AOJ16xx/AOJ1610/BambooBlossoms.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <climits>
+#include <fstream>
+using namespace std;
+inline int toInt(string s) { int v; istringstream sin(s); sin >> v; return v; }
+template<class T> inline string toStr(T x) { ostringstream sout; sout << x; return sout.str(); }
+typedef vector<int> vi;
+typedef vector<vi>  vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define REP(i,n) FOR(i,0,(n)-1)
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+const int INF = INT_MAX / 10;
+const int MOD = 1000000007;
+
+int main() {
+	int m, n;
+	while (cin >> m >> n, m) {
+		vector<char> num(8000000, 1);
+		int k = 0, p = m;
+		while (k < n) {
+			for (int i = p; i < 8000000; i += p) {
+				num[i] = 0;
+			}
+			while (num[p] == 0) {
+				p++;
+			}
+			k++;
+		}
+		cout << p << endl;
+	}
+}
