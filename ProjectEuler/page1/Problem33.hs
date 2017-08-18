42b34787732f197511b7ed1d5164fc997e4ca788
Ryohei Takasawa <henshinfukanou@gmail.com>
2017-08-18 13:55:33 +0900

    Modify Problem 33 using Ratio.

--- ProjectEuler/page1/Problem33.hs
+++ ProjectEuler/page1/Problem33.hs
@@ -1,23 +1,12 @@
-isEqual :: (Int, Int) -> (Int, Int) -> Bool
-isEqual (a, b) (c, d) = a*d == b*c
+import Data.Ratio
 
-cancelDigit :: (Int, Int) -> (Int, Int)
-cancelDigit (x, y)
-	| a == c 	= (b, d)
-	| a == d 	= (b, c)
-	| b == c 	= (a, d)
-	| b == d 	= (a, c)
-	| otherwise	= (-1, -1)
+canSimplify :: (Int, Int) -> Bool
+canSimplify (a, b) = ((x == y) && (a%b == w%z))
 	where
-		a = x `mod` 10
-		b = x `div` 10
-		c = y `mod` 10
-		d = y `div` 10
+		(w, x) = divMod a 10
+		(y, z) = divMod b 10
 
-
-main = print $ (numerator, denominator)
+main = print $ product fractions
 	where
 		list = filter (\x -> x `mod` 10 /= 0) [11 .. 99]
-		fractions = [cancelDigit (a, b) | a <- list, b <- list, a < b, isEqual (a, b) (cancelDigit (a, b))]
-		numerator = product $ map fst fractions
-		denominator = product $ map snd fractions
+		fractions = [a%b | a <- list, b <- list, a < b, canSimplify (a, b)]
