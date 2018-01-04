ef73c7204a7f4cee61926b045c0f3f3903740360
Ryohei Takasawa <henshinfukanou@gmail.com>
2018-01-04 16:57:59 +0900

    Add AOJ2745 file.

---
+++ AizuOnlineJugde/AOJ27xx/AOJ2745/CurryMaking.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <climits>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+inline int toInt(string s) { int v; istringstream sin(s); sin >> v; return v; }
+template<class T> inline string toStr(T x) { ostringstream sout; sout << x; return sout.str(); }
+typedef vector<int> vi;
+typedef vector<vi>  vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define ALL(a) (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(),(a).rend()
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define REP(i,n) FOR(i,0,(n)-1)
+const double EPS = 1e-10;
+const double PI = acos(-1.0);
+const int INF = INT_MAX / 2;
+const int MOD = 1000000007;
+
+int main() {
+	int R0, W0, C, R;
+	while (cin >> R0 >> W0 >> C >> R, R0) {
+		int X = 0;
+		while (R0 + X*R < C*W0) {
+			X++;
+		}
+		cout << X << endl;
+	}
+	return 0;
+}
